6791a36620746610730b751688eaf84608891eeb
Jyri Lehtinen <jyri.j.lehtinen@iki.fi>
2017-05-04 13:11:45 +0300

    OpenGL library added

---
+++ Segmentation/inc/rg_ogl.hpp
@@ -0,0 +1,13 @@
+#ifndef RG_OGL_HPP
+#define RG_OGL_HPP
+
+#include <stdio.h>
+#include <iostream>
+
+#include <GL/glew.h>
+#include <glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtx/transform.hpp>
+#include "shader.hpp"
+
+#endif //RG_OGL_HPP
