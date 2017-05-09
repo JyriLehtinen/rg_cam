597f0d344bbfbdb518a5b7a80dba78bbf9523a55
Jyri Lehtinen <jyri.j.lehtinen@iki.fi>
2017-05-09 11:14:33 +0300

    Added ogl libraries

--- Segmentation/inc/rg_ogl.hpp
+++ Segmentation/inc/rg_ogl.hpp
@@ -1,6 +1,9 @@
 #ifndef RG_OGL_HPP
 #define RG_OGL_HPP
 
+#define GLFW_INCLUDE_GL3  /* don't drag in legacy GL headers. */
+#define GLFW_NO_GLU  /* Don't drag in GLU, unless you really must */
+
 #include <stdio.h>
 #include <iostream>
 
